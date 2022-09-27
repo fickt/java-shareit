13b95a3054ebd532357ed1b97a18526e7edfdf18
Michael Arbuzov <garrys2machinima@gmail.com>
2022-09-27 17:36:10 +0700

    v1.4 checkstyle fix

--- src/main/java/ru/practicum/shareit/item/comment/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/item/comment/dto/CommentDto.java
@@ -4,7 +4,6 @@ package ru.practicum.shareit.item.comment.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
