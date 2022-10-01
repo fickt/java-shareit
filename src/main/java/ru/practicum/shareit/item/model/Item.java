71a20351dc2a8205f7d8fec478dbbb17f24dae24
Michael Arbuzov <garrys2machinima@gmail.com>
2022-10-01 20:54:05 +0700

    comment functionality removed test

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,16 +1,12 @@
 package ru.practicum.shareit.item.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
-import ru.practicum.shareit.item.comment.model.Comment;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @Builder
@@ -22,14 +18,14 @@ public class Item {
 
     }
 
-    public Item(Long id, Long ownerId, String name, String description, Boolean isAvailable, Long requestId, List<Comment> comments) {
+    public Item(Long id, Long ownerId, String name, String description, Boolean isAvailable, Long requestId) {
         this.id = id;
         this.ownerId = ownerId;
         this.name = name;
         this.description = description;
         this.isAvailable = isAvailable;
         this.requestId = requestId;
-        this.comments = comments;
+      //  this.comments = comments;
     }
 
     @Id
@@ -47,7 +43,7 @@ public class Item {
     private Boolean isAvailable;
     @Column(name = "REQUEST_ID")
     private Long requestId;
-    @OneToMany(mappedBy = "item")
-    @JsonManagedReference
-    private List<Comment> comments = new ArrayList<>();
+   // @OneToMany(mappedBy = "item")
+   // @JsonManagedReference
+    //private List<Comment> comments = new ArrayList<>();
 }
--- src/main/java/ru/practicum/shareit/item/rowmapper/ItemDtoRowMapper.java
+++ src/main/java/ru/practicum/shareit/item/rowmapper/ItemDtoRowMapper.java
@@ -1,13 +1,11 @@
 package ru.practicum.shareit.item.rowmapper;
 
 import org.springframework.jdbc.core.RowMapper;
-import ru.practicum.shareit.item.comment.converter.CommentDtoConverter;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -43,10 +41,10 @@ public class ItemDtoRowMapper implements RowMapper<ItemDto> {
                 .ownerId(item.getOwnerId())
                 .description(item.getDescription())
                 .isAvailable(item.getIsAvailable())
-                .comments(item.getComments() != null ?
-                        item.getComments().stream()
-                                .map(CommentDtoConverter::convertCommentToDto)
-                                .collect(Collectors.toList()) : Collections.emptyList()) //TODO UNCOMMITED CHANGES
+             //   .comments(item.getComments() != null ?
+           //             item.getComments().stream()
+           //                     .map(CommentDtoConverter::convertCommentToDto)
+            //                    .collect(Collectors.toList()) : Collections.emptyList()) //TODO UNCOMMITED CHANGES
                 .requestId(item.getRequestId())
                 .build();
     }
